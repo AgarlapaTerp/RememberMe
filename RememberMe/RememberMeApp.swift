a987d9b526a91bfe04c4c153cb1160202821b746
abgbro <codepath@abhigarlapati.com>
2024-05-01 21:27:32 -0400

    Initial Commit

---
+++ RememberMe/RememberMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberMeApp.swift
+//  RememberMe
+//
+//  Created by user256510 on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RememberMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
